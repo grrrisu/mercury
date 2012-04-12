e504ec40d7196ad55dbd812e51dd36154b37d58f
Alessandro Di Maria <adm@m42.ch>
2012-04-12 23:29:06 +0200

    upgrade to paperclip 3.0

--- lib/generators/mercury/install/images/templates/mongoid_paperclip_image.rb
+++ lib/generators/mercury/install/images/templates/mongoid_paperclip_image.rb
@@ -6,6 +6,11 @@ class Mercury::Image
 
   validates_presence_of :image
 
+  # upgrade to paperclip 3.0:app/models/mercury/image.rb
+  # has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" },
+  #       :path => ":rails_root/public/system/:attachment/:id/:style/:filename",
+  #       :url => "/system/:attachment/:id/:style/:filename"
+
   delegate :url, :to => :image
 
   def serializable_hash(options = nil)
